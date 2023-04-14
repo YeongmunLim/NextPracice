72fec92323e81d2af4515870925d797166962ef8
YeongmunLim <newmin123@naver.com>
2023-04-14 15:47:42 +0900

    note updated

--- next.config.js
+++ next.config.js
@@ -3,6 +3,36 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/products/deleted_forever",
+        destination: "/products",
+        permanent: true,
+      },
+      {
+        source: "/products/deleted_temp",
+        destination: "/products",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/items/:slug",
+        destination: "/products/:slug",
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
